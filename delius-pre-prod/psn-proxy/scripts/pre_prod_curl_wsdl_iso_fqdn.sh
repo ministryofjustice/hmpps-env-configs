1c4fc834bb5bf8b956b772981893b633af303b95
Paul Crimes <paul.crimes@solirius.com>
2019-11-26 18:00:53 +0000

    added notes as to why 1 of the urls wouldnt curl

--- delius-pre-prod/psn-proxy/scripts/pre_prod_curl_wsdl_iso_fqdn.sh
+++ delius-pre-prod/psn-proxy/scripts/pre_prod_curl_wsdl_iso_fqdn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "shows spg iso wsdl as seen by CRC stub"
+echo "shows spg iso wsdl as seen by psn proxy using psn-int certs"
 curl --key /etc/pki/tls/private/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.key \
      --cert /etc/pki/tls/certs/spgw-int-psn.pre-prod.probation.service.justice.gov.uk.public-certificate.pem \
      --cacert /etc/pki/tls/certs/privateca.pre-prod.probation.service.justice.gov.uk.public-certificate.pem \
-     https://spgw-int-psn.pre-prod.probation.service.justice.gov.uk:9001/cxf/InboundSPGProxy/1.2?wsdl -v
+     https://spgw-ext.pre-prod.probation.service.justice.gov.uk:9001/cxf/InboundSPGProxy/1.2?wsdl -v
--- delius-prod/psn-proxy/scripts/production_curl_wsdl_iso_fqdn.sh
+++ delius-prod/psn-proxy/scripts/production_curl_wsdl_iso_fqdn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "shows spg iso wsdl as seen by CRC stub hosted in AWS"
+echo "shows spg iso wsdl as seen by PSN proxy"
 curl --key /etc/pki/tls/private/spgw-int-psn.probation.service.justice.gov.uk.key \
      --cert /etc/pki/tls/certs/spgw-int-psn.probation.service.justice.gov.uk.public-certificate.pem \
      --cacert /etc/pki/tls/certs/privateca.probation.service.justice.gov.uk.public-certificate.pem \
-     https://spgw-int-psn.probation.service.justice.gov.uk:9001/cxf/InboundSPGProxy/1.2?wsdl -v
+     https://spgw-ext.probation.service.justice.gov.uk:9001/cxf/InboundSPGProxy/1.2?wsdl -v
