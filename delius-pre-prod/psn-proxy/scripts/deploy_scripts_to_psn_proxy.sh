b0fe051a93928bd80629bc4c6eb83315036f9da3
Paul Crimes <paul.crimes@solirius.com>
2020-09-03 13:58:57 +0100

    refactor deploy scripts to copy whole directory and prompt administrator to take further action

--- delius-pre-prod/psn-proxy/scripts/deploy_scripts_to_psn_proxy.sh
+++ delius-pre-prod/psn-proxy/scripts/deploy_scripts_to_psn_proxy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 #this script requires that the user has appropriate ssh config and bastion-prod access
 chmod u+x *.sh
-scp *.sh ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/
-scp *.sh ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/
+
+# nginx config scripts for stage (untested)
+scp - r ../../  ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/
+
+echo "log onto server and as root, copy the config directory into /etc/nginx/"
+
+echo "reload nginx configs & confirm service is operational with..."
+
+echo "systemnctl reload nginx && systemnctl status nginx"
--- delius-stage/psn-proxy/scripts/deploy_scripts_to_psn_proxy_stage.sh
+++ delius-stage/psn-proxy/scripts/deploy_scripts_to_psn_proxy_stage.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 #this script requires that the user has appropriate ssh config and bastion-prod access
 chmod u+x *.sh
-scp *.sh ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/
-scp *.sh ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/
 
-# nginx config scripts for stage
-scp ../im-proxy-stage.conf ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/im-proxy-stage.conf
-scp ../im-proxy-stage.conf ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/im-proxy-stage.conf
+# nginx config scripts for stage (untested)
+scp - r ../../  ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/
 
-scp ../spg-proxy-stage-ext.conf ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/spg-proxy-stage-ext.conf
-scp ../spg-proxy-stage-ext.conf ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/spg-proxy-stage-ext.conf
+echo "log onto server and as root, copy the config directory into /etc/nginx/"
 
-scp ../spg-proxy-stage-int.conf ec2-user@hmpps-delius-psn-test-a:/opt/hmpps/proxy-scripts/spg-proxy-stage-int.conf
-scp ../spg-proxy-stage-int.conf ec2-user@hmpps-delius-psn-test-b:/opt/hmpps/proxy-scripts/spg-proxy-stage-int.conf
+echo "reload nginx configs & confirm service is operational with..."
 
-# LetsEncrypt configuration for *.stage.psn.probation.service.justice.gov.uk
-scp ../etc/letsencrypt/renewal/stage.psn.probation.service.justice.gov.uk.conf ec2-user@hmpps-delius-psn-test-a:/etc/letsencrypt/renewal/stage.psn.probation.service.justice.gov.uk.conf
-scp ../etc/letsencrypt/renewal/stage.psn.probation.service.justice.gov.uk.conf ec2-user@hmpps-delius-psn-test-b:/etc/letsencrypt/renewal/stage.psn.probation.service.justice.gov.uk.conf
-
-#reload nginx configs & confirm service reloaded configs ok
-systemnctl reload nginx
-systemnctl status nginx
+echo "systemnctl reload nginx && systemnctl status nginx"
